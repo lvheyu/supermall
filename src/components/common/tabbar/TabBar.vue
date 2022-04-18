1687582d7677ba776436a17a34818b2dd59ae235
lvheyu <lvheyu@163.com>
2022-04-18 09:47:32 +0800

    初始化项目

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="tab-bar" class="tab-bar">
+  <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar"
+}
+</script>
+
+<style scoped>
+/*定义tab-bar样式*/
+#tab-bar {
+  display:flex;
+  background-color: #f6f6f6;
+  /*tabbar移到底部*/
+  position: fixed;
+  left:0;
+  right:0;
+  bottom:0;
+
+  /*x水平移动,y垂直移动,高度，rgba(颜色，透明度)*/
+  box-shadow: 0 -3px 1px rgba(100,100,100,.1);
+}
+
+</style>
